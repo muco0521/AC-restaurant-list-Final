42f2844048a24b565caa2f45001955173395cffa
muco0521 <muco0521@gmail.com>
2023-04-26 15:04:16 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,28 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Restaurant = require('./models/restaurant')
 const handlebarsHelper = require('./config/handlebars-helper')
-
-if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config()
-}
-
-//set MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('MongoDB error')
-})
-
-db.once('open', () => {
-  console.log('MongoDB connected!')
-})
-
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs', handlebarsHelper: handlebarsHelper }))
 app.set('view engine', 'hbs')
---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB error')
+})
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
+
+module.exports = db
