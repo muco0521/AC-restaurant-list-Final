f14f9421cccca9dda643f855ad3edcbc639d5200
muco0521 <muco0521@gmail.com>
2023-06-14 10:46:21 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,4 +4,11 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/login', (req, res) => {
+})
+
 module.exports = router
