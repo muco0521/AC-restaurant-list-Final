4a0f06c5be36e27288cf0107f9772900b1c277cd
muco0521 <muco0521@gmail.com>
2023-06-14 14:44:23 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const handlebarsHelper = require('./config/handlebars-helper')
 const routes = require('./routes')
@@ -13,10 +14,16 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs', handlebarsHelper }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
