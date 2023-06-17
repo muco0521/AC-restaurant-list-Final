0b447abcb38cc5fbab3980d3079556c5061c6f33
muco0521 <muco0521@gmail.com>
2023-06-17 20:56:27 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,6 +10,11 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: 'users/login'
