e155f68bac830a285708f4fa5c5dd9cf24a56afe
muco0521 <muco0521@gmail.com>
2023-06-22 18:22:58 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const handlebarsHelper = require('./config/handlebars-helper')
 const routes = require('./routes')
@@ -23,10 +24,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,9 +10,14 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/users/login')
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    req.flash('success_msg', '您已經成功登出')
+    res.redirect('/users/login')
+  })
 })
 
 router.post('/login', passport.authenticate('local', {
@@ -22,20 +27,36 @@ router.post('/login', passport.authenticate('local', {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填的' })
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符' })
+  }
+
+  if (errors.length) {
+    return res.render('register', {
+      errors, name, email, password, confirmPassword
+    })
+  }
+
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('User already exists.')
-        res.render('register', {
-          name, email, password, confirmPassword
+        errors.push({ message: '此信箱已經被註冊過了' })
+        return res.render('register', {
+          errors, name, email, password, confirmPassword
         })
-      } else {
-        return User.create({
-          name, email, password, confirmPassword 
-        })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
       }
+
+      return User.create({
+        name, email, password, confirmPassword 
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 })
