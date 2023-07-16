e47bd8a5e781ed8feabfdc082478904e744166fe
muco0521 <muco0521@gmail.com>
2023-07-16 13:45:01 +0800

    refactor: modify restaurantSeeder

--- Models/seeds/restaurantSeeder.js
+++ Models/seeds/restaurantSeeder.js
@@ -1,9 +1,29 @@
+const Restaurant = require('../restaurant.js')
+const { users, restaurants } = require('./data')
 const db = require('../../config/mongoose')
-const Restaurant = require('../restaurant')
-const restaurantList = require('../../restaurant.json').results
+const User = require('../user')
+const bcrypt = require('bcryptjs')
 
-db.once('open', () => {
-  Restaurant.create(restaurantList)
-    .then(() => console.log('done'))
-    .catch(() => console.log('error'))
+db.once('open', async () => {
+  await Promise.all(
+    users.map(async (user, user_index) => {
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(user.password, salt)
+      user.password = hash
+      const createdUser = await User.create({ ...user })
+      console.log('user created!')
+
+      const userRestaurant = []
+      restaurants.forEach((restaurant, rest_index) => {
+        if (rest_index < (user_index + 1) * 3 && rest_index >= user_index * 3) {
+          restaurant.userId = createdUser._id
+          userRestaurant.push(restaurant)
+        }
+      })
+      await Restaurant.create(userRestaurant)
+      console.log('restaurant created!')
+    })
+  )
+  console.log('done')
+  process.exit()
 })
